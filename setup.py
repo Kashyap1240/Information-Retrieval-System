f0c2a8cbbf0657097bcfff7d30c146be0904e9c0
Kashyap1240 <shivamskashyap@gmail.com>
2025-02-19 15:53:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Infromation Reterival System Using Gen-AI',
+    version= '0.0.0',
+    author= 'Shivam Kashyap',
+    author_email= 'shivamskashyap@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
